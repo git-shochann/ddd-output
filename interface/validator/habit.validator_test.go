c6f0706c49484b11195728d76ad9a03de4f59ed5
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-11-12 19:07:24 +0900

    add testing for validation

--- interface/validator/habit.validator_test.go
+++ interface/validator/habit.validator_test.go
@@ -1,11 +1,30 @@
 package validator
 
 import (
+	"ddd/domain/model"
 	"testing"
 )
 
 func TestCreateHabitValidator(t *testing.T) {
-	// fmt.Println("first test")
-	// *model.CreateHabitValidationを作る
+
+	habitValidation := NewHabitValidation()
+
+	// 正常時のパターン
+	createHabitValidation := model.CreateHabitValidation{
+		Content: "testtest",
+	}
+	message, _ := habitValidation.HabitValidate(&createHabitValidation)
+	if message != "" {
+		t.Fatal("failed test")
+	}
+
+	// contentが空白の場合
+	createHabitValidation = model.CreateHabitValidation{
+		Content: "",
+	}
+	message, _ = habitValidation.HabitValidate(&createHabitValidation)
+	if message != "Invalid Content" {
+		t.Fatal("failed test")
+	}
 
 }
