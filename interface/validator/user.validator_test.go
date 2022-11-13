87fe0e6d11559718e4b9e110bb0abb3fe06b72a4
git-shochann <sho_825.sa216@docomo.ne.jp>
2022-11-13 15:25:15 +0900

    wip testing

--- interface/validator/user.validator_test.go
+++ interface/validator/user.validator_test.go
@@ -22,6 +22,45 @@ func TestSignupValidate(t *testing.T) {
 		t.Fatalf("\n実際: %v\n理想: %v", result, expect)
 	}
 
+	// missing last name
+	userSignUpValidation = model.UserSignUpValidation{
+		FirstName: "Taro",
+		LastName:  "",
+		Email:     "taro@gmail.com",
+		Password:  "taro0123456",
+	}
+	expect = "Invalid Last Name"
+	result, _ = userValidation.SignupValidate(&userSignUpValidation)
+	if result != expect {
+		t.Fatalf("\n実際: %v\n理想: %v", result, expect)
+	}
+
+	// missing email
+	userSignUpValidation = model.UserSignUpValidation{
+		FirstName: "Taro",
+		LastName:  "Taro",
+		Email:     "",
+		Password:  "taro0123456",
+	}
+	expect = "Invalid Email"
+	result, _ = userValidation.SignupValidate(&userSignUpValidation)
+	if result != expect {
+		t.Fatalf("\n実際: %v\n理想: %v", result, expect)
+	}
+
+	// missing password
+	userSignUpValidation = model.UserSignUpValidation{
+		FirstName: "Taro",
+		LastName:  "Taro",
+		Email:     "taro@gmail.com",
+		Password:  "",
+	}
+	expect = "Invalid Password"
+	result, _ = userValidation.SignupValidate(&userSignUpValidation)
+	if result != expect {
+		t.Fatalf("\n実際: %v\n理想: %v", result, expect)
+	}
+
 }
 
 // Signin
